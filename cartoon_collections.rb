f30abc039b6a55d94e5999f347d5adfc12878585
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 02:58:39 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,6 @@ def roll_call_dwarves (array)
     array.each_with_index do |name, index|
     puts "#{index + 1} #{name}"
   end
-  # code an argument here
-
-  # Your code here
 end
 
 def summon_captain_planet(array)
@@ -14,23 +11,19 @@ def summon_captain_planet(array)
       caped << string.capitalize + "!"
      end
      caped
-  # code an argument here
-  # Your code here
 end
 
 def long_planeteer_calls (array)
-    new=[]
-    array.map do |string|
-    if  string.count("") > 4
-    
-      return true
-    else false
 
+    array.map do |string|
+    if  string.count > 4
+        true
+    else false
       end
    end
 end
 
 def find_the_cheese (array)
     array.map do |
-
+    end
 end
