0061cf9e9975b77592ac815c8b8d24b3af583af1
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 03:34:21 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,17 +13,19 @@ def summon_captain_planet(array)
      caped
 end
 
-def long_planeteer_calls (array)
-
-    array.map do |string|
-    if  string.count > 4
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |string|
+    if  string.length > 4
         true
-    else false
-      end
-   end
+    else
+      false
+    end
+  end
 end
 
 def find_the_cheese (array)
-    array.map do |
+    cheese = ["cheddar", "gouda", "camembert"]
+    array.find do |snack|
+      cheese.include?(snack)
     end
 end
